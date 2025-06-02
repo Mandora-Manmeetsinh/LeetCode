e247e747d0e4fa9d6f7e981869ec5f6c8a7726b3
Mandora Manmeetsinh Dashrathsinh <157201046+Mandora-Manmeetsinh@users.noreply.github.com>
2025-06-02 08:05:30 +0530

    Time: 52 ms (32.07%), Space: 19.7 MB (85.9%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        while(slow):
+        	if slow.val == None:
+        		return True
+        	slow.val = None 
+        	slow = slow.next
+        return False
