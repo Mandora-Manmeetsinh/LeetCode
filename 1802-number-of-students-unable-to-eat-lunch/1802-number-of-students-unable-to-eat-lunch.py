04b67583dacf65db3bc0580a761816845a38597c
Mandora Manmeetsinh Dashrathsinh <157201046+Mandora-Manmeetsinh@users.noreply.github.com>
2025-06-11 08:27:48 +0530

    Time: 0 ms (100%), Space: 12.4 MB (81.31%) - LeetHub

---
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+class Solution:
+    def countStudents(self, students , sandwiches) :
+        res = len(students)
+        cnt = Counter(students)
+
+        for s in sandwiches:
+            if cnt[s] > 0:
+                res -= 1
+                cnt[s] -= 1
+            else:
+                return res
+        return res
