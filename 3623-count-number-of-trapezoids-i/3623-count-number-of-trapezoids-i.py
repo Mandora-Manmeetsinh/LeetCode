cacbb0eb9444904f8c52a9c28963d1ee4804c0a0
Mandora Manmeetsinh Dashrathsinh <157201046+Mandora-Manmeetsinh@users.noreply.github.com>
2025-12-02 19:51:29 +0530

    Time: 79 ms (70.31%), Space: 63.7 MB (26.19%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countTrapezoids(self, points):
+        M = 10**9 + 7          
+        y_count = {}            
+
+        for x, y in points:
+            if y in y_count:
+                y_count[y] += 1
+            else:
+                y_count[y] = 1
+        
+        counts = list(y_count.values())  
+        r = 0                            
+        s = 0
+        for c in counts:
+            if c >= 2:
+                w = c * (c - 1) // 2
+                r += w * s
+                s += w
+        
+        return r % M
